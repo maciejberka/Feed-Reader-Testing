e0905bb482fd8e753f16f86b06d6fd3a9b8dc52c
maciejberka <maciejberka@gmail.com>
2018-06-03 11:38:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+#Feed Reader Testing
+##Udacity Front-End Nanodegree program. Project #5
+
+###About
+
+The goal of this project was to write tests against Feed Reader App made by Udacity using Jasmine framework. You can see my work in spec/feedreader.js file.
+
+###My tests:
+
+1. RSS Feeds
+  *Are defined
+  *Has defined URLS
+  *Has defined names
+  
+2. Menu
+  *Is hidden by default
+  *Is visible/invisible after clicking menu icon
+  
+3. Initial entries
+  *Feed contain at least one entry element
+  
+4. New feed selection
+  *Content is changing when a new feed is loaded
+
+###Install
+
+1. Download all files:
+  *Use green 'clone or download' button.
+    *Download ZIP file.
+    *Clone repo using Git.
+    
+2. Navigate to the downloaded folder.
+
+3. Find html.index file and open it by web browser.
+
+###Authors
+
+*[Maciej Berka](https://github.com/maciejberka) (tests in feedreader.js file)
+*[Udacity](https://github.com/udacity) (Feed Reader App) [See original repo](https://github.com/udacity/frontend-nanodegree-feedreader)
+
+###License
+
+This project is licensed under the MIT License.
+
+###Thank you:
+
+*Udacity - for great course and help
+*Google - for the scholarship
+*Mentor - for sharing your knowledge
+*Reviewer - for a lot of suggestions
+
+
+
+
+
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,115 @@
+
+//Run tests when DOM is ready
+$(function() {
+    
+    //Tests against feed
+    describe('RSS Feeds', function() {
+        //Is allFeeds variable defined and not empty?
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+      
+        //Did objects in allFeeds have defined URLS?
+        it('has defined URLS', function(){
+          for(var i = 0; i < allFeeds.length; i++){
+            expect(allFeeds[i].url).toBeTruthy();
+          }
+        });    
+        
+        //Did objects in allFeeds have defined names?
+        it('has defined names', function(){
+          for(var i = 0; i < allFeeds.length; i++){
+            expect(allFeeds[i].name).toBeTruthy();
+          }
+        });
+      
+    });
+
+    //Tests against menu
+    describe('The menu', function(){
+      
+        //Is slide-menu hidden by default? 
+        it('is hidden by default', function(){
+          var body = document.querySelector('body');
+          var isHidden = body.classList.contains('menu-hidden');
+          expect(isHidden).toBe(true);
+        });      
+      
+        //Is menu visible after click on menu icon and invisible after second click? 
+        it('visibility of menu is changed when the menu icon is clicked', function(){
+          var menuIcon = document.querySelector('.menu-icon-link');
+          var body = document.querySelector('body');
+          
+          //Is menu visible after first click?
+          menuIcon.click();
+          var isHidden = body.classList.contains('menu-hidden');
+          expect(isHidden).toBe(false);
+          
+          //Is menu invisible after second click?
+          menuIcon.click();
+          isHidden = body.classList.contains('menu-hidden');
+          expect(isHidden).toBe(true);
+        });
+      
+    });  
+  
+    //Tests against initial entries
+    describe('Initial Entries', function(){
+        
+        //loadFeed function is asynchronous, so we have to make our tests when loadFeed complete its work
+        beforeEach(function(done) {
+          loadFeed(0, done);
+        });
+        
+        //loadFeed function is done. Did feed contain at least one .entry element?
+        it('.feed container contain at least one .entry element', function(){
+          expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+      
+    });
+  
+    //Tests against new feed selection
+    describe('New Feed Selection', function(){
+        //Is content changing when a new feed is loaded?
+      
+        //We will store headings in those variables to compare it later
+        var firstHeader;
+        var secondHeader; 
+
+        //loadFeed is asynchronous, so wait until its complete its work...
+        beforeEach(function(done){
+          loadFeed(0, function(){
+            //...and take first header of first feed to variable  
+            firstHeader = document.querySelector('.feed h2').innerHTML;
+          });
+
+          //Wait until loadFeed load second feed...
+          loadFeed(1, function() {
+            //...and take first header of second feed to variable 
+            secondHeader = document.querySelector('.feed h2').innerHTML;
+            
+            done();
+          });
+ 
+        });
+
+        //Is content changing when a new feed is loaded?
+        it('Content is changing when a new feed is loaded', function(done) {
+          //Compare firstHeader and secondHeader. They should be different, because we switched feeds.
+          expect(firstHeader).not.toEqual(secondHeader);
+          done();
+        });
+
+    });  
+  
+}());
+
+
+
+
+
+
+
+
+
